42e2b521c25795118fa382835a1b4779241e0795
Ceejay Cicala <S4170883@student.rmit.edu.au>
2026-01-08 23:11:59 +1100

    Fixed blur height and implemented Lenis

--- src/script.js
+++ src/script.js
@@ -1,3 +1,14 @@
+// ~~~ LENIS ~~~
+// Initialize Lenis
+const lenis = new Lenis({
+    autoRaf: true,
+});
+
+// Listen for the scroll event and log the event data
+lenis.on('scroll', (e) => {
+    console.log(e);
+});
+
 // header
 class THeader extends HTMLElement {
     connectedCallback() {
